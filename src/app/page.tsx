5f7f674a680d977e139cb24e74110a44b37034d1
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-12-01 22:20:09 +0530

    done blinks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,14 +24,14 @@ export default function Page() {
 	return (
 		<div className="w-screen h-screen flex flex-col items-center bg-background">
 			<div className="w-full h-3/5 flex flex-col justify-center items-center">
-				<img src="black-bg.png" className="w-2/5"/>
+				<img src="back.gif" className="w-2/5"/>
 				<h1 className="text-9xl text-accent">{displayText}</h1>
 			</div>
 			<div className="w-full h-2/5 flex justify-center items-center">
 				<button
 					className="bg- text-white text-3xl border-4 border-[#17B8BD] p-5 rounded-lg hover:bg-gray-800"	
 					onClick={()=>{
-						window.location.href = "https://dial.to/?action=solana-action%3Ahttp%3A%2F%2Flocalhost%3A3000%2Fapi%2Factions%2Fcreate-game&cluster=devnet"
+						window.location.href = "https://dial.to/?action=solana-action:https://scramble.nixblack.site/api/actions/create-roulette&cluster=devnet"
 					}}
 				>
 					Create a challenge
